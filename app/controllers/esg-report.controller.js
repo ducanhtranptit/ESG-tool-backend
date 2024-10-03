2e748c469b10ab2542e7c9dd9b077617ed0e0f19
ducanhtranptit <tda.ducanh@gmail.com>
2024-10-03 14:13:05 +0700

    update code

--- app/controllers/esg-report.controller.js
+++ app/controllers/esg-report.controller.js
@@ -4,7 +4,7 @@ import EsgReportAction from "../actions/esg-report.action.js";
 class EsgReportController {
 	static async calculateESGReport(req, res) {
 		try {
-			await EsgReportAction.calculateESGReport();
+			EsgReportAction.calculateESGReport();
 			return new SuccessResponse().send(req, res);
 		} catch (error) {
 			console.error(error);
