32fc5ce9be86079e5013311e1fdfd814b5ed8d00
ducanhtranptit <tda.ducanh@gmail.com>
2024-10-31 10:33:41 +0700

    update pie chart

--- app/actions/dashboard.action.js
+++ app/actions/dashboard.action.js
@@ -2,47 +2,47 @@ import model from "../models/index.js";
 
 export default class DashboardAction {
 	static async getAllDataForCharts(userId) {
-		const userInfor = await model.User.findOne({
-			where: {
-				id: userId,
-			},
-		});
+		const userInfor = await model.User.findOne({ where: { id: userId } });
 		const companyId = userInfor.dataValues.companyId;
 		const companyInfor = await model.Company.findOne({
-			where: {
-				id: companyId,
-			},
+			where: { id: companyId },
 		});
 		const companyCode = companyInfor.dataValues.companyCode;
 		const companyScoreESG = await model.CompanyScore.findAll({
-			where: {
-				companyCode: companyCode,
-			},
+			where: { companyCode },
 		});
-		const data = [];
-		for (let companyScoreESGCurrentYear of companyScoreESG) {
-			const companyData = {
-				year: companyScoreESGCurrentYear.dataValues.year,
-				esg: companyScoreESGCurrentYear.dataValues.esgScore,
-				esgRank: companyScoreESGCurrentYear.dataValues.esgRank,
-				environmental:
-					companyScoreESGCurrentYear.dataValues.environmentScore,
-				environmentRank:
-					companyScoreESGCurrentYear.dataValues.environmentRank,
-				social: companyScoreESGCurrentYear.dataValues.socialScore,
-				socialRank: companyScoreESGCurrentYear.dataValues.socialRank,
-				governance:
-					companyScoreESGCurrentYear.dataValues.governanceScore,
-				governanceRank:
-					companyScoreESGCurrentYear.dataValues.governanceRank,
-			};
-			data.push(companyData);
-		}
 
-		const result = {
-			company: { name: companyCode, data: data },
-		};
+		const pillarWeights = await Promise.all(
+			[1, 2, 3].map((pillarId) =>
+				model.Criteria.findOne({
+					where: {
+						applicableIndustryCode:
+							companyInfor.dataValues.industryCodeLevel2,
+						pillarId,
+					},
+					attributes: ["pillarWeight"],
+				})
+			)
+		);
 
-		return result;
+		const [environmentWeight, socialWeight, governanceWeight] =
+			pillarWeights.map((w) => w.pillarWeight);
+
+		const data = companyScoreESG.map((score) => ({
+			year: score.dataValues.year,
+			esg: score.dataValues.esgScore,
+			esgRank: score.dataValues.esgRank,
+			environmental: score.dataValues.environmentScore,
+			environmentRank: score.dataValues.environmentRank,
+			environmentWeight,
+			social: score.dataValues.socialScore,
+			socialRank: score.dataValues.socialRank,
+			socialWeight,
+			governance: score.dataValues.governanceScore,
+			governanceRank: score.dataValues.governanceRank,
+			governanceWeight,
+		}));
+
+		return { company: { name: companyCode, data } };
 	}
 }
